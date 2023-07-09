10840fb924ecedf0a0f07bf8f450be1b37713377
Maera <baeitsmae@gmail.com>
2023-07-09 13:48:55 +0200

    details

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+
 import Advantages from "./components/Advantages"
 import Bestsellers from "./components/Bestsellers"
 import Plants from "./components/Plants"
 import Header from "./components/Header"
+import Soldes from "./components/Soldes"
+
 
 function App() {
+
   return (
     <>
       <Header />
+      <Soldes />
       <Bestsellers />
       <Advantages />
       <Plants />
